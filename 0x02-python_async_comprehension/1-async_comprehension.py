e9b368061f535ec25404b7acb01f9b299543d7a8
CHEGEBB <chegephil24@gmail.com>
2024-07-10 09:06:31 +0300

    added new

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python3
-""" a python module to returns 10 random numbers using async comprehension"""
+"""
+This module contains the function for task 1 of the project
+"""
 from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
     """
-    async_comprehension- function to return 10 random numbers
-    Arguments:
-        no arguments
-    Returns:
-        10 random numbers
+    This function will collect 10 random numbers using an async generator
+    and return them in a list
     """
     result = [i async for i in async_generator()]
     return result
--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -9,12 +9,8 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
-    '''Executes async_comprehension 4 times and measures the
-    total execution time.
-    Arguments:
-        No arguments
-    Returns:
-        runtime
+    '''This function will measure the runtime of async_comprehension
+    four times and return the total runtime.
     '''
     start_time = time.time()
     await asyncio.gather(*(async_comprehension() for _ in range(4)))
